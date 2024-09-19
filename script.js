da0ac71ce22c15d04539adaffe080bd71435b29d
Amanda <amanda_aguiarfontes@hotmail.com>
2024-09-19 00:01:03 -0300

    Modificações na página

--- script.js
+++ script.js
@@ -261,3 +261,34 @@ function adicionarDocumento() {
 function limparFormulario(formId) {
     document.getElementById(formId).reset();
 }
+
+var target = document.getElementById('target');
+var watchId;
+
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
+
+if ('DeviceOrientationEvent' in window) {
+    window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+  } else {
+    document.getElementById('logoContainer').innerText = 'Device Orientation API not supported.';
+  }
+  
+  function deviceOrientationHandler (eventData) {
+    var tiltLR = eventData.gamma;
+    var tiltFB = eventData.beta;
+    var dir = eventData.alpha;
+    
+    document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
+    document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
+    document.getElementById("doDirection").innerHTML = Math.round(dir);
+  
+    var logo = document.getElementById("imgLogo");
+    logo.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+    logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
+    logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
+  }
--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,6 @@ self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open('lawease-store').then((cache) => cache.addAll([
             '/index.html',
-            '/pages/agenda.html',
-            '/pages/cadastro.html',
-            '/pages/clientes.html',
-            '/pages/documentos.html',
-            '/pages/login.html',
-            '/pages/processos.html',
             '/assets/logo.png',
             'icons/icon-87x87.png',
             'icons/icon-180x180.png',
