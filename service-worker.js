4b217b401cbf9684025869c7c4011648e752706d
Amanda <amanda_aguiarfontes@hotmail.com>
2024-09-19 00:35:47 -0300

    Modificação PWA

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-// self.addEventListener('install', (e) => {
-//     e.waitUntil(
-//         caches.open('lawease-store').then((cache) => cache.addAll([
-//             '/index.html',
-//             '/assets/logo.png',
-//             'icons/icon-87x87.png',
-//             'icons/icon-180x180.png',
-//             'icons/icon-512x512.png',
-//             '/styles.css',
-//             '/script.js',
-//             '/manifest.json'
-//         ]))
-//     );
-// });
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('lawease-store').then((cache) => cache.addAll([
+            '/index.html',
+            '/assets/logo.png',
+            'icons/icon-87x87.png',
+            'icons/icon-180x180.png',
+            'icons/icon-512x512.png',
+            '/styles.css',
+            '/script.js',
+            '/manifest.json'
+        ]))
+    );
+});
 
-// self.addEventListener('fetch', (e) => {
-//     e.respondWith(
-//         caches.match(e.request).then((response) => response || fetch(e.request))
-//     );
-// });
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
